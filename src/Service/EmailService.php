52143023f04baf3fad46c4d82c25dda5296a7f7e
mithridatem <mathieumithridate@adrar-formation.com>
2025-01-21 13:57:39 +0100

    feat : configEmail

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -8,47 +8,55 @@ use PHPMailer\PHPMailer\Exception;
 
 class EmailService
 {
+    private readonly PHPMailer $mail;
     public function __construct(
         private string $userEmail,
         private string $pwdEmail,
         private string $smtpEmail,
         private int $portSmtpEmail
-    ) {}
+    ) {
+        $this->mail = new PHPMailer(true); 
+    }
+    
+    //Méthode pour envoyer les emails
+    public function sendEmail(string $destinataire, string $objet, string $body) {
+        
+        try {
+            //Configuration
+            $this->configEmail();
 
-    public function test() {
-        return $this->userEmail . ", " . $this->pwdEmail . ", " . $this->smtpEmail . ", " .  $this->portSmtpEmail; 
+            //Expéditeur
+            $this->mail->setFrom($this->userEmail, 'Mailer');
+            //Destinataire
+            $this->mail->addAddress($destinataire);     //Add a recipient
+
+            //Contenu du mail
+            //Format HTML
+            $this->mail->isHTML(true);
+            //Objet du mail                                  //Set email format to HTML
+            $this->mail->Subject = $objet;
+            //Contenu du mail
+            $this->mail->Body    = $body;
+            //Envoi le mail
+            $this->mail->send();
+            
+            return 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}";
+        }
     }
 
-
-    public function sendEmail(string $destinataire, string $objet, string $body) {
-        //Create an instance; passing `true` enables exceptions
-        $mail = new PHPMailer(true);
-
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = $this->smtpEmail;                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = $this->userEmail;                     //SMTP username
-            $mail->Password   = $this->pwdEmail;                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = $this->portSmtpEmail;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-            //Recipients
-            $mail->setFrom($this->userEmail, 'Mailer');
-            $mail->addAddress($destinataire);     //Add a recipient
-
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = $objet;
-            $mail->Body    = $body;
-            //Envoi le mail
-            $mail->send();
-           return 'Message has been sent';
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
+    //Méthode pour configurer l'envoi de mail
+    public function configEmail() {
+        //Server settings
+        $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $this->mail->isSMTP();                                            //Send using SMTP
+        $this->mail->Host       = $this->smtpEmail;                     //Set the SMTP server to send through
+        $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $this->mail->Username   = $this->userEmail;                     //SMTP username
+        $this->mail->Password   = $this->pwdEmail;                               //SMTP password
+        $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $this->mail->Port       = $this->portSmtpEmail;     
     }
 }
 
