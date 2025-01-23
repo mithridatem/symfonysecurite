317dcd8db526b0b46e58daeac6f1c0039c218dc9
mithridatem <mathieumithridate@adrar-formation.com>
2025-01-23 10:30:34 +0100

    feat : ApiController

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+class ApiController extends AbstractController
+{
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {}
+
+    
+}
