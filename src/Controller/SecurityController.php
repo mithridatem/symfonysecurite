e88fd91fabf584682d9a8dd5a60732b46ed46cdd
mithridatem <mathieumithridate@adrar-formation.com>
2025-01-23 09:15:13 +0100

    feat : form-user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+
 class SecurityController extends AbstractController
 {
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {}
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -17,7 +23,14 @@ class SecurityController extends AbstractController
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
+        if($lastUsername!= null){
+            $user = $this->userRepository->findOneBy(["email"=>$lastUsername]);
+            if(!$user->isActived()){
+                $this->addFlash('danger', "Le compte n'est pas activé");
+                return $this->redirectToRoute('app_home'); 
+                
+            }
+        }
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
